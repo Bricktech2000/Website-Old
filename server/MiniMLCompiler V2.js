ae9c46591793e8ef1188203d98a870c86e154ac3
Bricktech2000 <Bricktech2000@gmail.com>
2020-08-20 19:40:46 -0400

    added MiniMLCompiler prototype which returns its source code

--- server/MiniMLCompiler V2.js
+++ server/MiniMLCompiler V2.js
@@ -1,6 +1,37 @@
 var miniML = {
-  compile: function(){
-    return 'miniML!';
+  compile: function(_src){
+    var src = _src.text || _src;
+    var ast = parse(src);
+    var html = gen(ast);
+    return html; //_compile(code, 'miniML');
   },
+};
+var parse = function(src, stx = 'miniML'){
+  var syntax = {
+    miniML: /^(inlineCode|blockCode|[^])*/,
+    inlineCode: /^\[code\]/,
+    blockCode: /^\[\[code\]\]/,
+    code: /(codeKeyword|codeOperator|miniML|[^])*/,
+    codeKeyword: /^\b(if|then|else|end|while|break|continue|for|var|return|function|class|and|or|not)\b/,
+    codeOperator: /^([\[\]\+\-\*\/=%\?\^\.:\(\)\/,]|&lt;|&gt;|<<|>>)/,
+  }
+  //https://stackoverflow.com/questions/39154255/converting-regexp-to-string-then-back-to-regexp
+  var keys = ''; //syntax[stx].source;
+  for(var key of Object.keys(syntax))
+    //regex = regex.replace(new RegExp(stx, 'g'), '[^]{0, }')
+    keys += key + '|';
+  keys += '_';
+  keys = new RegExp(keys, 'g');
+
+  var syntax = syntax[stx].source.replace(keys, (match) => 'SPLIT' + match + 'SPLIT');
+  var regexArr = syntax.split('SPLIT');
+  console.log(regexArr);
+  /*return code.replace(syntax[stx], function(a){
+    return a;
+  });*/
+  return src;
+}
+var gen = function(ast){
+  return ast; //`<pre>${JSON.stringify(ast).replace(/\\n/g, '\\n\n')}</pre>`;
 }
 export { miniML }
